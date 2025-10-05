6dcdc736333f83c6d4ccb9950759937117707e1c
Prashant Kumar Maurya <mauryaprashant252@gmail.com>
2025-10-06 00:47:42 +0530

    changing my docker file

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserService } from 'src/user/user.service';
 import { RegisterDto } from './dto/registerUser.dto';
+import { loginDto } from './dto/loginUser.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -12,5 +13,11 @@ export class AuthController {
         const user = await this.authService.registerUser(registerUserDto)
 
         return user
+    }
+
+    @Post('login')
+    async login(@Body() loginUserDto:loginDto){
+        const {user,token} = await this.authService.loginUser(loginUserDto)
+        return {user,token}
     }
 }
